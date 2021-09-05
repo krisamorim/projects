668e10d3a78344bc56bd5959c2efae7dce54d6ed
krisamorim <kris.a.furtado@gmail.com>
2021-09-05 20:47:36 +0000

    Fields' validation improved and submit add in the button tag

--- portal01/clocker/pages/index.js
+++ portal01/clocker/pages/index.js
@@ -43,25 +43,25 @@ export default function Home() {
         <FormControl id="email" p={4} isRequired> 
           <FormLabel>Email</FormLabel>
           <Input type="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/> 
-          <FormHelperText textColor="#e74c3c">{formik.errors.email}</FormHelperText>
+          {formik.touched.email && <FormHelperText textColor="#e74c3c">{formik.errors.email}</FormHelperText>}
         </FormControl>
 
         <FormControl id="password" p={4} isRequired>
           <FormLabel>Password</FormLabel>
           <Input type="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
-          <FormHelperText textColor="#e74c3c">{formik.errors.password}</FormHelperText>
+          {formik.touched.password && <FormHelperText textColor="#e74c3c">{formik.errors.password}</FormHelperText>}
         </FormControl>
 
         <Box display="flex" flexDirection="row" alignItems="center">
           <Text> clocker.work/</Text>
           <FormControl id="username" p={4} isRequired>
             <Input type="username" value={formik.values.username} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
-            <FormHelperText textColor="#e74c3c">{formik.errors.username}</FormHelperText>
+            {formik.touched.username && <FormHelperText textColor="#e74c3c">{formik.errors.username}</FormHelperText>}
           </FormControl> 
         </Box>
 
         <Box p={4}>
-          <Button width="100%">Go ahead</Button>
+          <Button width="100%" onClick={formik.handleSubmit}>Go ahead</Button>
         </Box>
 
 

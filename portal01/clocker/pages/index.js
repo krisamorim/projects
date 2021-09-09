e4ba0eb02527e1415de52849fd850cbbeb01b65a
krisamorim <kris.a.furtado@gmail.com>
2021-09-09 19:01:55 +0000

    revert

--- portal01/clocker/pages/index.js
+++ portal01/clocker/pages/index.js
@@ -1,6 +1,3 @@
-import { useFormik } from 'formik'
-import * as yup from 'yup'
-
 import {
   Container, 
   Box, 
@@ -14,30 +11,7 @@ import {
 
 import { Logo } from './../components'
 
-const validationSchema = yup.object().shape({
-  email: yup.string().email('Not valid E-mail').required('This field is required'),
-  password: yup.string().required('This field is required'),
-  username: yup.string().required('This field is required'),
-})
-
 export default function Home() {
-  const {
-    values,
-    errors,
-    touched,
-    handleChange,
-    handleBlur,
-    handleSubmit
-  } = useFormik({
-    onSubmit: () => { },
-    validationSchema,
-    initialValues: {
-      email: '',
-      username: '',
-      password: ''
-    }
-  })
-  
   return (
     <Container p={4} centerContent>
       <Logo />
@@ -49,26 +23,23 @@ export default function Home() {
       <Box> 
         <FormControl id="email" p={4} isRequired> 
           <FormLabel>Email</FormLabel>
-          <Input type="email" value={values.email} onChange={handleChange} onBlur={handleBlur}/> 
-          {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>}
+          <Input type="email" /> 
         </FormControl>
 
         <FormControl id="password" p={4} isRequired>
           <FormLabel>Password</FormLabel>
-          <Input type="password" value={values.password} onChange={handleChange} onBlur={handleBlur}/>
-          {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
+          <Input type="password" />
         </FormControl>
 
         <Box display="flex" flexDirection="row" alignItems="center">
           <Text> clocker.work/</Text>
           <FormControl id="username" p={4} isRequired>
-            <Input type="username" value={values.username} onChange={handleChange} onBlur={handleBlur}/>
-            {touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText>}
+            <Input type="username" />
           </FormControl> 
         </Box>
 
         <Box p={4}>
-          <Button width="100%" onClick={handleSubmit}>Go ahead</Button>
+          <Button width="100%">Go ahead</Button>
         </Box>
 
 
